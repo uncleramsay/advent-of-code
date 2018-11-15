5f5564dafb68bb48fe93edcdb3865b352c9df763
Dale Caffull <dale@caffull.co.uk>
2018-11-15 04:28:25 +0000

    feat(env): setting up environment

--- 2016/index.js
+++
@@ -1,7 +0,0 @@
-const day = process.argv[2];
-const part = process.argv[3];
-
-const solutions = require(`./${day}/solutions`);
-const solution = solutions[part]();
-
-console.log(`Day ${day} Part ${part}: ${solution}`);
---
+++ bin/scaffold.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const join = require('path').join;
+
+if (process.argv.length < 4) {
+  console.log('Usage: node scaffold <year> <day>');
+  process.exit();
+}
+
+const year = process.argv[2];
+const day = process.argv[3];
+
+const path = join(__dirname, `../${year}/${day}`);
+
+fs.mkdirSync(path, { recursive: true });
+
+fs.writeFileSync(
+  `${path}/solutions.js`,
+  fs.readFileSync(join(__dirname, 'solutions.tpl'))
+);
+
+fs.writeFileSync(`${path}/data.txt`, '');
